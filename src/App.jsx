f31ce5fea27732eda2393d161cd038b0eab43de0
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-11-12 12:15:50 +0100

    checking the data extraction with a btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
 
 export default function App() {
 
+  const [actorsList, setActorList] = useState([])
+
+  const endpointActor = "https://lanciweb.github.io/demo/api/actresses/"
+  const endpointActress = "https://lanciweb.github.io/demo/api/actresses/"
+
+  function getDataActor() {
+    axios.get(endpointActor)
+      .then(res => setActorList(res.data))
+      ;
+  }
+
+  useEffect(getDataActor, [])
+
+
   return (
     <>
-
+      <header className='bg-dark'>
+        <h1 className='text-white text-center py-4'>ACTORPEDIA</h1>
+        <button onClick={() => console.log(actorsList)}></button>
+      </header>
     </>
   )
 }
